4ce946cfd51415bd72c3d43e7a259334bf36af09
github_FacundoGB <bardifacundo@gmail.com>
2022-07-14 19:56:11 -0300

    added api idnetification to url. Created delete function that tales user id dynamically

--- src/main/java/com/JavaWebSerice/WebService/controllers/UserController.java
+++ src/main/java/com/JavaWebSerice/WebService/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.JavaWebSerice.WebService.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -16,7 +17,7 @@ public class UserController {
     @Autowired
     private UserDao userDao;
 
-    @RequestMapping(value = "user/{id}")
+    @RequestMapping(value = "api/user/{id}")
     public User getUser(@PathVariable Long id) {
         User user = new User();
         user.setId(id);
@@ -28,7 +29,7 @@ public class UserController {
         return user;
     }
     //returns full list of users
-    @RequestMapping(value = "users")
+    @RequestMapping(value = "api/users")
     public List<User> listUsers() {
         return userDao.listUsers();
     }
@@ -42,15 +43,9 @@ public class UserController {
         user.setPhone("341555555");
         return user;
     }
-    @RequestMapping(value = "userDel")
-    public User deleteUser() {
-        User user = new User();
-        user.setName("Faucndo");
-        user.setSurname("Bardi");
-        user.setEmail("facundo@mail.com");
-        user.setPassword("12341234");
-        user.setPhone("341555555");
-        return user;
+    @RequestMapping(value = "api/user/{id}", method = RequestMethod.DELETE)
+    public void deleteUser(@PathVariable long id) {
+        userDao.delete(id);
     }
 
     @RequestMapping(value = "userSearch")
