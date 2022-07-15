2cdd74703e1e85a785d93dc9e548c11c2791f91a
github_FacundoGB <bardifacundo@gmail.com>
2022-07-15 17:05:24 -0300

    implemented password encryption. Should use exception handlers.

--- src/main/java/com/JavaWebSerice/WebService/controllers/UserController.java
+++ src/main/java/com/JavaWebSerice/WebService/controllers/UserController.java
@@ -2,6 +2,8 @@ package com.JavaWebSerice.WebService.controllers;
 
 import com.JavaWebSerice.WebService.dao.UserDao;
 import com.JavaWebSerice.WebService.models.User;
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,8 +32,19 @@ public class UserController {
     public List<User> listUsers() {
         return userDao.listUsers();
     }
+
     @RequestMapping(value = "api/users", method = RequestMethod.POST)
     public void registerUser(@RequestBody User user) {
+
+        Argon2 encrypt = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id, 16, 32);
+        char [] password = user.getPassword().toCharArray();
+        String hash = encrypt.hash(1, 1024, 1, password);
+        /*
+            https://en.wikipedia.org/wiki/Argon2
+            https://mkyong.com/java/java-password-hashing-with-argon2/
+            https://stackoverflow.com/questions/66594009/java-argon2-hashing Try this one later better security
+         */
+        user.setPassword(hash);
         userDao.register(user);
     }
 
